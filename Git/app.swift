5244a1799f3739bf1d53eaddd96284b47ef421cb
teyit <=>
2024-08-11 17:33:16 +0600

    add changes for master branch

--- Git/app.swift
+++ Git/app.swift
@@ -2,3 +2,4 @@ print("add & commit current file"
 print("use git --staged : for showing changes at index"))
 print("git restore app.swift")
 print("git restore --staged app.swift : undo if at index")
+print("this changes only on master branch")
